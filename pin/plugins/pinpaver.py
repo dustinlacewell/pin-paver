0223c609bc8cad3c96996ee1357c010e2ade90da
Dustin Lacewell <dlacewell@gmail.com>
2011-04-13 11:15:41 -0500

    Fixed non-inited bug

--- pin/plugins/pinpaver.py
+++ pin/plugins/pinpaver.py
@@ -17,21 +17,24 @@ class PinPaverCommand(command.PinBaseCommandDelegator):
     def get_subcommands(cls):
         cwd = os.getcwd()
         root = get_project_root(cwd)
-        sys.path.append(root)
-        mod = __import__('pavement')
-        env = Environment(mod)
-        tasks = env.get_tasks()
-        maxlen, tasklist = _group_by_module(tasks)
-        for name, group in tasklist:
-            if name == 'pavement':
-                return dict((t.shortname, t) for t in group)
+        if root:
+            sys.path.append(root)
+            mod = __import__('pavement')
+            env = Environment(mod)
+            tasks = env.get_tasks()
+            maxlen, tasklist = _group_by_module(tasks)
+            for name, group in tasklist:
+                if name == 'pavement':
+                    return dict((t.shortname, t) for t in group)
 
     def setup_parser(self, parser):
         parser.add_argument('paverargs', nargs='*')
 
     def execute(self, cwd, root):
-        os.chdir(root)
-        print "echo %s" % self.options.paverargs
-        main(self.options.paverargs)
+        if root:
+            os.chdir(root)
+            print "echo %s" % self.options.paverargs
+            main(self.options.paverargs)
+            return True
 
 command.register(PinPaverCommand)
